1065c71be6da3a513c87b090433f6cdcdd563057
Roma <bamkarnov@mail.ru>
2024-04-08 19:29:10 +0300

    first

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Проектная работа Mesto
 
+## Ссылка на репозиторий
+
+[Репозиторий проекта](https://github.com/RoKa781/mesto-project-ff.git)
+
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const cardList = document.querySelector('.places__list');
+const addButton = document.querySelector('.profile__add-button');
+const closeButton = document.querySelector('.popup_type_new-card .popup__close');
+const popupForm = document.querySelector('.popup.popup_type_new-card');
 // @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
+function addCards(name, link) {
+    const cardItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+    cardItem.querySelector('.card__title').textContent = name;
+    cardItem.querySelector('.card__image').src = link;
+    cardList.append(cardItem);
+    // @todo: Функция удаления карточки
+    cardItem.querySelector('.card__delete-button').addEventListener('click', function (evt) {
+        evt.target.closest('.places__item').remove();
+    });
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (card) {
+    addCards(card.name, card.link)
+});
+
+addButton.addEventListener('click', function () {
+    popupForm.classList.add('popup_is-opened');
+});
+
+closeButton.addEventListener('click', function () {
+    popupForm.classList.remove('popup_is-opened');
+});
